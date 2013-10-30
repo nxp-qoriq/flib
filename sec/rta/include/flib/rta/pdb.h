645b1b7412d5934221aeada4fcfc46b6dea5932a
Oana Barbu <Oana.Barbu@freescale.com>
2013-10-30 13:59:10 +0200

    add RSA protocol descriptors
    Add RSA descriptors for encryption and decryption in
    protoshared library. SEC implements the decrypt operation
    in 3 forms, depending on how the private key is provided.
    Only form #1 decrypt descriptor is tested with simple_proto
    application.
    
    Change-Id: Ia34489d113ed5596bfcc7c0d59b28a625559ced7
    Signed-off-by: Oana Barbu <Oana.Barbu@freescale.com>
    Reviewed-on: http://git.am.freescale.net:8181/6305
    Reviewed-by: Horia Ioan Geanta Neag <horia.geanta@freescale.com>
    Tested-by: Horia Ioan Geanta Neag <horia.geanta@freescale.com>

--- sec/rta/include/flib/rta/pdb.h
+++ sec/rta/include/flib/rta/pdb.h
@@ -423,4 +423,129 @@ struct wifi_decap_pdb {
 	uint16_t ctr_init;	/* CCM Counter block init counter */
 };
 
+#define RSA_ENC_SGF_SHIFT	28
+#define RSA_ENC_RSV_SHIFT	24
+#define RSA_ENC_E_LEN_SHIFT	12
+
+struct rsa_encrypt_pdb_64b {
+	uint32_t header;	/* Contains sgf, rsv, #e, #n fields */
+	uint32_t f_ref_high;	/* Reference to input */
+	uint32_t f_ref_low;
+	uint32_t g_ref_high;	/* Reference to output */
+	uint32_t g_ref_low;
+	uint32_t n_ref_high;	/* Reference to modulus */
+	uint32_t n_ref_low;
+	uint32_t e_ref_high;	/* Reference to public key */
+	uint32_t e_ref_low;
+	uint32_t f_len;		/* Input length */
+};
+
+struct rsa_encrypt_pdb {
+	uint32_t header;	/* Contains sgf, rsv, #e, #n fields */
+	uint32_t f_ref;		/* Reference to input */
+	uint32_t g_ref;		/* Reference to output */
+	uint32_t n_ref;		/* Reference to modulus */
+	uint32_t e_ref;		/* Reference to public key */
+	uint32_t f_len;		/* Input length */
+};
+
+#define RSA_DEC1_SGF_SHIFT		28
+#define RSA_DEC1_RSV_SHIFT		24
+#define RSA_DEC1_D_LEN_SHIFT	12
+
+struct rsa_decrypt_pdb_form1_64b {
+	uint32_t header;	/* Contains sgf, rsv, #d, #n fields */
+	uint32_t g_ref_high;	/* Reference to input */
+	uint32_t g_ref_low;
+	uint32_t f_ref_high;	/* Reference to output */
+	uint32_t f_ref_low;
+	uint32_t n_ref_high;	/* Reference to modulus */
+	uint32_t n_ref_low;
+	uint32_t d_ref_high;	/* Reference to private key */
+	uint32_t d_ref_low;
+};
+
+struct rsa_decrypt_pdb_form1 {
+	uint32_t header;	/* Contains sgf, rsv, #d, #n fields */
+	uint32_t g_ref;		/* Reference to input */
+	uint32_t f_ref;		/* Reference to output */
+	uint32_t n_ref;		/* Reference to modulus */
+	uint32_t d_ref;		/* Reference to private key */
+};
+
+#define RSA_DEC2_SGF_SHIFT		25
+#define RSA_DEC2_D_LEN_SHIFT	12
+#define RSA_DEC2_Q_LEN_SHIFT	12
+
+struct rsa_decrypt_pdb_form2_64b {
+	uint32_t header;	/* Contains sgf, rsv, #d, #n fields */
+	uint32_t g_ref_high;	/* Reference to input */
+	uint32_t g_ref_low;
+	uint32_t f_ref_high;	/* Reference to output */
+	uint32_t f_ref_low;
+	uint32_t d_ref_high;	/* Reference to private key */
+	uint32_t d_ref_low;
+	uint32_t p_ref_high;	/* Reference to prime p */
+	uint32_t p_ref_low;
+	uint32_t q_ref_high;	/* Reference to prime q */
+	uint32_t q_ref_low;
+	uint32_t tmp1_ref_high;	/* Reference to tmp1 */
+	uint32_t tmp1_ref_low;
+	uint32_t tmp2_ref_high;	/* Reference to tmp2 */
+	uint32_t tmp2_ref_low;
+	uint32_t trailer;	/* Contains rsv, #q, #p fields */
+};
+
+struct rsa_decrypt_pdb_form2 {
+	uint32_t header;	/* Contains sgf, rsv, #d, #n fields */
+	uint32_t g_ref;		/* Reference to input */
+	uint32_t f_ref;		/* Reference to output */
+	uint32_t d_ref;		/* Reference to private key */
+	uint32_t p_ref;		/* Reference to prime p */
+	uint32_t q_ref;		/* Reference to prime q */
+	uint32_t tmp1_ref;	/* Reference to tmp1 */
+	uint32_t tmp2_ref;	/* Reference to tmp2 */
+	uint32_t trailer;	/* Contains rsv, #q, #p fields */
+};
+
+#define RSA_DEC3_SGF_SHIFT		25
+#define RSA_DEC3_Q_LEN_SHIFT	24
+
+struct rsa_decrypt_pdb_form3_64b {
+	uint32_t header;	/* Contains sgf, rsv, #n fields */
+	uint32_t g_ref_high;	/* Reference to input */
+	uint32_t g_ref_low;
+	uint32_t f_ref_high;	/* Reference to output */
+	uint32_t f_ref_low;
+	uint32_t c_ref_high;	/* Reference to c */
+	uint32_t c_ref_low;
+	uint32_t p_ref_high;	/* Reference to prime p */
+	uint32_t p_ref_low;
+	uint32_t q_ref_high;	/* Reference to prime q */
+	uint32_t q_ref_low;
+	uint32_t dp_ref_high;	/* Reference to dp */
+	uint32_t dp_ref_low;
+	uint32_t dq_ref_high;	/* Reference to dq */
+	uint32_t dq_ref_low;
+	uint32_t tmp1_ref_high;	/* Reference to tmp1 */
+	uint32_t tmp1_ref_low;
+	uint32_t tmp2_ref_high;	/* Reference to tmp2 */
+	uint32_t tmp2_ref_low;
+	uint32_t trailer;	/* Contains rsv, #q, #p fields */
+};
+
+struct rsa_decrypt_pdb_form3 {
+	uint32_t header;	/* Contains sgf, rsv, #n fields */
+	uint32_t g_ref;		/* Reference to input */
+	uint32_t f_ref;		/* Reference to output */
+	uint32_t c_ref;		/* Reference to c */
+	uint32_t p_ref;		/* Reference to prime p */
+	uint32_t q_ref;		/* Reference to prime q */
+	uint32_t dp_ref;	/* Reference to dp */
+	uint32_t dq_ref;	/* Reference to dq */
+	uint32_t tmp1_ref;	/* Reference to tmp1 */
+	uint32_t tmp2_ref;	/* Reference to tmp2 */
+	uint32_t trailer;	/* Contains rsv, #q, #p fields */
+};
+
 #endif /* __RTA_PDB_H__ */
