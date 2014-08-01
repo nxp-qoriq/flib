3f0b06f2625798c0a086ab5dbf2871af92ba7e39
Horia Geanta <horia.geanta@freescale.com>
2014-08-01 16:38:30 +0300

    add functions for computing MDHA split key (pad) length
    Move MDHA split key length computation out of jobdesc.h.
    The functionality is needed in other places too.
    
    Signed-off-by: Horia Geanta <horia.geanta@freescale.com>
    Reviewed-on: http://git.am.freescale.net:8181/15433
    Reviewed-by: Alexandru Porosanu <alexandru.porosanu@freescale.com>
    Change-Id: I6b7bcdb5dd5ce30b03dc29c0ece60233bf7a1282
    Reviewed-on: http://git.am.freescale.net:8181/22741

--- sec/rta/include/flib/desc/common.h
+++ sec/rta/include/flib/desc/common.h
@@ -3,6 +3,8 @@
 #ifndef __DESC_COMMON_H__
 #define __DESC_COMMON_H__
 
+#include "flib/rta.h"
+
 /**
  * DOC: Shared Descriptor Constructors - shared structures
  *
@@ -36,4 +38,34 @@ struct protcmd {
 	uint16_t protinfo;
 };
 
-#endif /* __DESC_ALGO_H__ */
+/**
+ * split_key_len - Compute MDHA split key length for a given algorithm
+ * @hash: Hashing algorithm selection, one of OP_ALG_ALGSEL_* - MD5, SHA1,
+ *        SHA224, SHA384, SHA512.
+ *
+ * Return: MDHA split key length
+ */
+static inline uint32_t split_key_len(uint32_t hash)
+{
+	/* Sizes for MDHA pads (*not* keys): MD5, SHA1, 224, 256, 384, 512 */
+	static const uint8_t mdpadlen[] = { 16, 20, 32, 32, 64, 64 };
+	uint32_t idx;
+
+	idx = (hash & OP_ALG_ALGSEL_SUBMASK) >> OP_ALG_ALGSEL_SHIFT;
+
+	return (uint32_t)(mdpadlen[idx] * 2);
+}
+
+/**
+ * split_key_pad_len - Compute MDHA split key pad length for a given algorithm
+ * @hash: Hashing algorithm selection, one of OP_ALG_ALGSEL_* - MD5, SHA1,
+ *        SHA224, SHA384, SHA512.
+ *
+ * Return: MDHA split key pad length
+ */
+static inline uint32_t split_key_pad_len(uint32_t hash)
+{
+	return ALIGN(split_key_len(hash), 16);
+}
+
+#endif /* __DESC_COMMON_H__ */
