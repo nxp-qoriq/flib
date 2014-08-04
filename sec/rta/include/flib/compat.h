784ad8e807d0620918cd3045ce74475b988dd2c8
Horia Geanta <horia.geanta@freescale.com>
2014-08-04 18:37:16 +0300

    fix bool type usage in AIOP (#2)
    As it turns out, commit f90ea6b6860371acc3855808cc62ad6235867a0a
    ("fix bool type usage in AIOP") does not cover the following case:
    -C99 extensions are on (thus _Bool is considered keyword)
    -_EWL_C99 set to 0 (thus "bool", "true", "false" and
    "__bool_true_false_are_defined" macros won't be defined in stdbool.h)
    leading to compile error:
    =typedef unsigned char   _Bool;
    > declaration specifier conflict: illegal 'bool' sequence
    
    Make sure C99 extensions are not enabled before redefining _Bool.
    Also, if _EWL_C99 is set to 1, just include stdbool.h directly.
    
    Change-Id: I60610fc92429e41117676272666ece62c0d1b647
    Signed-off-by: Horia Geanta <horia.geanta@freescale.com>
    Reviewed-on: http://git.am.freescale.net:8181/15439

--- sec/rta/include/flib/compat.h
+++ sec/rta/include/flib/compat.h
@@ -19,11 +19,17 @@
 #include "common/fsl_stdio.h"
 #include "fsl_dbg.h"
 #include "fsl_endian.h"
+#if _EWL_C99
+#include <stdbool.h>
+#else
+#if !__option(c99)
 typedef unsigned char			_Bool;
+#endif
 #define bool				_Bool
 #define true				1
 #define false				0
 #define __bool_true_false_are_defined	1
+#endif /* _EWL_C99 */
 #else
 #error Environment not supported!
 #endif
