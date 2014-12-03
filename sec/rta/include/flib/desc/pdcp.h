2477bd9dcb649c72186ad916e633b0c1ce5d359f
Tudor Ambarus <tudor.ambarus@freescale.com>
2014-12-03 18:08:55 +0200

    pdcp: fix snow f8 & aes-cmac decap descriptor on ERA4
    Fix incorrect ERA handling. ERA4 was missed from descriptor logic
    and an internal register was overwritten with two different values.
    
    Change-Id: I9ab0834ad04e134bcead7012ff4aac6cf8360dbb
    Signed-off-by: Tudor Ambarus <tudor.ambarus@freescale.com>
    Reviewed-on: http://git.am.freescale.net:8181/25328
    Reviewed-by: Mircea Pop <mircea.pop@freescale.com>
    Reviewed-by: Horia Ioan Geanta Neag <horia.geanta@freescale.com>
    Tested-by: Horia Ioan Geanta Neag <horia.geanta@freescale.com>

--- sec/rta/include/flib/desc/pdcp.h
+++ sec/rta/include/flib/desc/pdcp.h
@@ -981,7 +981,7 @@ static inline int pdcp_insert_cplane_snow_aes_op(struct program *p,
 		KEY(p, KEY1, cipherdata->key_enc_flags, cipherdata->key,
 		    cipherdata->keylen, INLINE_KEY(cipherdata));
 
-		if (rta_sec_era >= RTA_SEC_ERA_5)
+		if (rta_sec_era >= RTA_SEC_ERA_4)
 			MOVE(p, CONTEXT1, 0, CONTEXT2, 0, 8, IMMED);
 		else
 			MOVE(p, CONTEXT1, 0, MATH3, 0, 8, IMMED);
@@ -1042,7 +1042,7 @@ static inline int pdcp_insert_cplane_snow_aes_op(struct program *p,
 
 		SEQFIFOLOAD(p, MSG1, 0, VLF | LAST1 | FLUSH1);
 
-		if (rta_sec_era >= RTA_SEC_ERA_5) {
+		if (rta_sec_era >= RTA_SEC_ERA_4) {
 			LOAD(p, NFIFOENTRY_STYPE_ALTSOURCE |
 			     NFIFOENTRY_DEST_CLASS1 |
 			     NFIFOENTRY_DTYPE_ICV |
