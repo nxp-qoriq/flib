nxp-qoriq/flib

commit 32be2d0f09f70ea5f91aca169d00c4802ef78c15

IPsec - fix PDB endianness
Fix C structures endianness for IPsec PDB, according to documentation.
Note that some fields are endianness-invariant (for e.g. big endian
irrespective of platform).

Other protocols besides IPsec (TLS, PDCP, SRTP, WiMAX, WiFi etc.)
will be fixed at a subsequent time. Right now, documentation pertaining
to PDB endianness is either missing or incomplete.

Change-Id: I4b5f12fb5c5c7eed6878779586acf6ccb6fb6a1a
Signed-off-by: Horia Geanta <horia.geanta@freescale.com>
Reviewed-on: http://git.am.freescale.net:8181/28068
Reviewed-by: Tudor-Dan Ambarus <tudor.ambarus@freescale.com>

sec/rta/include/flib/desc/ipsec.h
@@ -119,6 +119,11 @@
 /* IPSec ESP Decap PDB options */
 
 /**
+ * PDBOPTS_ESP_ARS_MASK - antireplay window mask
+ */
+#define PDBOPTS_ESP_ARS_MASK	0xc0
+
+/**
  * PDBOPTS_ESP_ARSNONE - No antireplay window
  */
 #define PDBOPTS_ESP_ARSNONE	0x00
@@ -178,8 +183,10 @@
  */
 #define PDBOPTS_ESP_ETU		0x01
 
-#define PDBHMO_ESP_DECAP_SHIFT	12
-#define PDBHMO_ESP_ENCAP_SHIFT	4
+#define PDBHMO_ESP_DECAP_SHIFT		28
+#define PDBHMO_ESP_ENCAP_SHIFT		28
+#define PDBHDRLEN_ESP_ENCAP_SHIFT	16
+#define PDBHDRLEN_MASK			(0x0fff << PDBHDRLEN_ESP_ENCAP_SHIFT)
 
 /**
  * PDBHMO_ESP_DECAP_DTTL - IPsec ESP decrement TTL (IPv4) / Hop limit (IPv6)
@@ -235,77 +242,71 @@
 
 /**
  * struct ipsec_encap_cbc - PDB part for IPsec CBC encapsulation
- * @iv: initialization vector
+ * @iv: 16-byte array initialization vector
  */
 struct ipsec_encap_cbc {
-	uint32_t iv[4];
+	uint8_t iv[16];
 };
 
 
 /**
  * struct ipsec_encap_ctr - PDB part for IPsec CTR encapsulation
- * @ctr_nonce: 4-byte nonce
+ * @ctr_nonce: 4-byte array nonce
  * @ctr_initial: initial count constant
  * @iv: initialization vector
  */
 struct ipsec_encap_ctr {
-	uint32_t ctr_nonce;
+	uint8_t ctr_nonce[4];
 	uint32_t ctr_initial;
-	uint32_t iv[2];
+	uint64_t iv;
 };
 
 /**
  * struct ipsec_encap_ccm - PDB part for IPsec CCM encapsulation
- * @salt: 3-byte salt (lower 24 bits)
- * @b0_flags: CCM B0; use 0x5B for 8-byte ICV, 0x6B for 12-byte ICV,
- *            0x7B for 16-byte ICV (cf. RFC4309, RFC3610)
- * @ctr_flags: counter flags; constant equal to 0x3
- * @ctr_initial: initial count constant
+ * @salt: 3-byte array salt (lower 24 bits)
+ * @ccm_opt: CCM algorithm options - MSB-LSB description:
+ *  b0_flags (8b) - CCM B0; use 0x5B for 8-byte ICV, 0x6B for 12-byte ICV,
+ *    0x7B for 16-byte ICV (cf. RFC4309, RFC3610)
+ *  ctr_flags (8b) - counter flags; constant equal to 0x3
+ *  ctr_initial (16b) - initial count constant
  * @iv: initialization vector
  */
 struct ipsec_encap_ccm {
-	uint32_t salt;
-	uint8_t b0_flags;
-	uint8_t ctr_flags;
-	uint16_t ctr_initial;
-	uint32_t iv[2];
+	uint8_t salt[4];
+	uint32_t ccm_opt;
+	uint64_t iv;
 };
 
 /**
  * struct ipsec_encap_gcm - PDB part for IPsec GCM encapsulation
- * @salt: 3-byte salt (lower 24 bits)
- * @rsvd1: reserved, do not use
+ * @salt: 3-byte array salt (lower 24 bits)
+ * @rsvd: reserved, do not use
  * @iv: initialization vector
  */
 struct ipsec_encap_gcm {
-	uint32_t salt;
-	uint32_t rsvd1;
-	uint32_t iv[2];
+	uint8_t salt[4];
+	uint32_t rsvd;
+	uint64_t iv;
 };
 
 /**
  * struct ipsec_encap_pdb - PDB for IPsec encapsulation
- * @hmo: header manipulation options
- * @options: option flags; depend on selected algorithm
+ * @options: MSB-LSB description (both for legacy and new modes)
+ *  hmo (header manipulation options) - 4b
+ *  reserved - 4b
+ *  next header (legacy) / reserved (new) - 8b
+ *  next header offset (legacy) / AOIPHO (actual outer IP header offset) - 8b
+ *  option flags (depend on selected algorithm) - 8b
  * @seq_num_ext_hi: (optional) IPsec Extended Sequence Number (ESN)
  * @seq_num: IPsec sequence number
  * @spi: IPsec SPI (Security Parameters Index)
- * @rsvd2: reserved, do not use
  * @ip_hdr_len: optional IP Header length (in bytes)
+ *  reserved - 16b
+ *  Opt. IP Hdr Len - 16b
  * @ip_hdr: optional IP Header content (only for IPsec legacy mode)
  */
 struct ipsec_encap_pdb {
-	uint8_t hmo;
-	union {
-		uint8_t ip_nh;	/* next header for legacy mode */
-		uint8_t rsvd;	/* reserved for new mode */
-	};
-	union {
-		uint8_t ip_nh_offset;	/* next header offset for legacy mode */
-		uint8_t aoipho;		/* actual outer IP header offset for
-					 * new mode */
-	};
-	uint8_t options;
+	uint32_t options;
 	uint32_t seq_num_ext_hi;
 	uint32_t seq_num;
 	union {
@@ -315,10 +316,59 @@ struct ipsec_encap_pdb {
 		struct ipsec_encap_gcm gcm;
 	};
 	uint32_t spi;
-	uint16_t rsvd2;
-	uint16_t ip_hdr_len;
-	uint32_t ip_hdr[0];
+	uint32_t ip_hdr_len;
+	uint8_t ip_hdr[0];
 };
+
+static inline unsigned __rta_copy_ipsec_encap_pdb(struct program *program,
+						  struct ipsec_encap_pdb *pdb,
+						  uint32_t algtype)
+{
+	unsigned start_pc = program->current_pc;
+
+	__rta_out32(program, pdb->options);
+	__rta_out32(program, pdb->seq_num_ext_hi);
+	__rta_out32(program, pdb->seq_num);
+
+	switch (algtype & OP_PCL_IPSEC_CIPHER_MASK) {
+	case OP_PCL_IPSEC_DES_IV64:
+	case OP_PCL_IPSEC_DES:
+	case OP_PCL_IPSEC_3DES:
+	case OP_PCL_IPSEC_AES_CBC:
+	case OP_PCL_IPSEC_NULL:
+		rta_copy_data(program, pdb->cbc.iv, sizeof(pdb->cbc.iv));
+		break;
+
+	case OP_PCL_IPSEC_AES_CTR:
+		rta_copy_data(program, pdb->ctr.ctr_nonce,
+			      sizeof(pdb->ctr.ctr_nonce));
+		__rta_out32(program, pdb->ctr.ctr_initial);
+		__rta_out64(program, pdb->ctr.iv);
+		break;
+
+	case OP_PCL_IPSEC_AES_CCM8:
+	case OP_PCL_IPSEC_AES_CCM12:
+	case OP_PCL_IPSEC_AES_CCM16:
+		rta_copy_data(program, pdb->ccm.salt, sizeof(pdb->ccm.salt));
+		__rta_out32(program, pdb->ccm.ccm_opt);
+		__rta_out64(program, pdb->ccm.iv);
+		break;
+
+	case OP_PCL_IPSEC_AES_GCM8:
+	case OP_PCL_IPSEC_AES_GCM12:
+	case OP_PCL_IPSEC_AES_GCM16:
+	case OP_PCL_IPSEC_AES_NULL_WITH_GMAC:
+		rta_copy_data(program, pdb->gcm.salt, sizeof(pdb->gcm.salt));
+		__rta_out32(program, pdb->gcm.rsvd);
+		__rta_out64(program, pdb->gcm.iv);
+		break;
+	}
+
+	__rta_out32(program, pdb->spi);
+	__rta_out32(program, pdb->ip_hdr_len);
+
+	return start_pc;
+}
 
 /**
  * struct ipsec_decap_cbc - PDB part for IPsec CBC decapsulation
@@ -330,55 +380,52 @@ struct ipsec_decap_cbc {
 
 /**
  * struct ipsec_decap_ctr - PDB part for IPsec CTR decapsulation
- * @salt: 4-byte salt
+ * @ctr_nonce: 4-byte array nonce
  * @ctr_initial: initial count constant
  */
 struct ipsec_decap_ctr {
-	uint32_t salt;
+	uint8_t ctr_nonce[4];
 	uint32_t ctr_initial;
 };
 
 /**
  * struct ipsec_decap_ccm - PDB part for IPsec CCM decapsulation
  * @salt: 3-byte salt (lower 24 bits)
- * @b0_flags: CCM B0; use 0x5B for 8-byte ICV, 0x6B for 12-byte ICV,
- *            0x7B for 16-byte ICV (cf. RFC4309, RFC3610)
- * @ctr_flags: counter flags
- * @ctr_initial: initial count constant
+ * @ccm_opt: CCM algorithm options - MSB-LSB description:
+ *  b0_flags (8b) - CCM B0; use 0x5B for 8-byte ICV, 0x6B for 12-byte ICV,
+ *    0x7B for 16-byte ICV (cf. RFC4309, RFC3610)
+ *  ctr_flags (8b) - counter flags; constant equal to 0x3
+ *  ctr_initial (16b) - initial count constant
  */
 struct ipsec_decap_ccm {
-	uint32_t salt;
-	uint8_t b0_flags;
-	uint8_t ctr_flags;
-	uint16_t ctr_initial;
+	uint8_t salt[4];
+	uint32_t ccm_opt;
 };
 
 /**
  * struct ipsec_decap_gcm - PDB part for IPsec GCN decapsulation
  * @salt: 4-byte salt
- * @resvd: reserved, do not used
+ * @rsvd: reserved, do not use
  */
 struct ipsec_decap_gcm {
-	uint32_t salt;
-	uint32_t resvd;
+	uint8_t salt[4];
+	uint32_t rsvd;
 };
 
 /**
  * struct ipsec_decap_pdb - PDB for IPsec decapsulation
- * @ip_hdr_len: HMO (upper nibble) + IP header length (lower 3 nibbles)
- * @options: option flags; depend on selected algorithm
+ * @options: MSB-LSB description (both for legacy and new modes)
+ *  hmo (header manipulation options) - 4b
+ *  IP header length - 12b
+ *  next header offset (legacy) / AOIPHO (actual outer IP header offset) - 8b
+ *  option flags (depend on selected algorithm) - 8b
  * @seq_num_ext_hi: (optional) IPsec Extended Sequence Number (ESN)
  * @seq_num: IPsec sequence number
- * @anti_replay: Anti-replay window bits array
+ * @anti_replay: Anti-replay window; size depends on ARS (option flags);
+ *  format must be Big Endian, irrespective of platform
  */
 struct ipsec_decap_pdb {
-	uint16_t ip_hdr_len;
-	union {
-		uint8_t ip_nh_offset;	/* next header offset for legacy mode */
-		uint8_t aoipho;		/* actual outer IP header offset for
-					 * new mode */
-	};
-	uint8_t options;
+	uint32_t options;
 	union {
 		struct ipsec_decap_cbc cbc;
 		struct ipsec_decap_ctr ctr;
@@ -389,6 +436,72 @@ struct ipsec_decap_pdb {
 	uint32_t seq_num;
 	uint32_t anti_replay[4];
 };
+
+static inline unsigned __rta_copy_ipsec_decap_pdb(struct program *program,
+						  struct ipsec_decap_pdb *pdb,
+						  uint32_t algtype)
+{
+	unsigned start_pc = program->current_pc;
+	unsigned i, ars;
+
+	__rta_out32(program, pdb->options);
+
+	switch (algtype & OP_PCL_IPSEC_CIPHER_MASK) {
+	case OP_PCL_IPSEC_DES_IV64:
+	case OP_PCL_IPSEC_DES:
+	case OP_PCL_IPSEC_3DES:
+	case OP_PCL_IPSEC_AES_CBC:
+	case OP_PCL_IPSEC_NULL:
+		__rta_out32(program, pdb->cbc.rsvd[0]);
+		__rta_out32(program, pdb->cbc.rsvd[1]);
+		break;
+
+	case OP_PCL_IPSEC_AES_CTR:
+		rta_copy_data(program, pdb->ctr.ctr_nonce,
+			      sizeof(pdb->ctr.ctr_nonce));
+		__rta_out32(program, pdb->ctr.ctr_initial);
+		break;
+
+	case OP_PCL_IPSEC_AES_CCM8:
+	case OP_PCL_IPSEC_AES_CCM12:
+	case OP_PCL_IPSEC_AES_CCM16:
+		rta_copy_data(program, pdb->ccm.salt, sizeof(pdb->ccm.salt));
+		__rta_out32(program, pdb->ccm.ccm_opt);
+		break;
+
+	case OP_PCL_IPSEC_AES_GCM8:
+	case OP_PCL_IPSEC_AES_GCM12:
+	case OP_PCL_IPSEC_AES_GCM16:
+	case OP_PCL_IPSEC_AES_NULL_WITH_GMAC:
+		rta_copy_data(program, pdb->gcm.salt, sizeof(pdb->gcm.salt));
+		__rta_out32(program, pdb->gcm.rsvd);
+		break;
+	}
+
+	__rta_out32(program, pdb->seq_num_ext_hi);
+	__rta_out32(program, pdb->seq_num);
+
+	switch (pdb->options & PDBOPTS_ESP_ARS_MASK) {
+	case PDBOPTS_ESP_ARS128:
+		ars = 4;
+		break;
+	case PDBOPTS_ESP_ARS64:
+		ars = 2;
+		break;
+	case PDBOPTS_ESP_ARS32:
+		ars = 1;
+		break;
+	case PDBOPTS_ESP_ARSNONE:
+	default:
+		ars = 0;
+		break;
+	}
+
+	for (i = 0; i < ars; i++)
+		__rta_out_be32(program, pdb->anti_replay[i]);
+
+	return start_pc;
+}
 
 /**
  * enum ipsec_icv_size - Type selectors for icv size in IPsec protocol
@@ -471,8 +584,8 @@ static inline int cnstr_shdsc_ipsec_encap(uint32_t *descbuf, bool ps, bool swap,
 	if (ps)
 		PROGRAM_SET_36BIT_ADDR(p);
 	phdr = SHR_HDR(p, SHR_SERIAL, hdr, 0);
-	COPY_DATA(p, (uint8_t *)pdb,
-		  sizeof(struct ipsec_encap_pdb) + pdb->ip_hdr_len);
+	__rta_copy_ipsec_encap_pdb(p, pdb, cipherdata->algtype);
+	COPY_DATA(p, pdb->ip_hdr, pdb->ip_hdr_len);
 	SET_LABEL(p, hdr);
 	pkeyjmp = JUMP(p, keyjmp, LOCAL_JUMP, ALL_TRUE, BOTH|SHRD);
 	KEY(p, MDHA_SPLIT_KEY, authdata->key_enc_flags, authdata->key,
@@ -525,7 +638,7 @@ static inline int cnstr_shdsc_ipsec_decap(uint32_t *descbuf, bool ps, bool swap,
 	if (ps)
 		PROGRAM_SET_36BIT_ADDR(p);
 	phdr = SHR_HDR(p, SHR_SERIAL, hdr, 0);
-	COPY_DATA(p, (uint8_t *)pdb, sizeof(struct ipsec_decap_pdb));
+	__rta_copy_ipsec_decap_pdb(p, pdb, cipherdata->algtype);
 	SET_LABEL(p, hdr);
 	pkeyjmp = JUMP(p, keyjmp, LOCAL_JUMP, ALL_TRUE, BOTH|SHRD);
 	KEY(p, MDHA_SPLIT_KEY, authdata->key_enc_flags, authdata->key,
@@ -593,8 +706,8 @@ static inline int cnstr_shdsc_ipsec_encap_des_aes_xcbc(uint32_t *descbuf,
 
 	PROGRAM_CNTXT_INIT(p, descbuf, 0);
 	phdr = SHR_HDR(p, SHR_SERIAL, hdr, 0);
-	COPY_DATA(p, (uint8_t *)pdb,
-		  sizeof(struct ipsec_encap_pdb) + pdb->ip_hdr_len);
+	__rta_copy_ipsec_encap_pdb(p, pdb, cipherdata->algtype);
+	COPY_DATA(p, pdb->ip_hdr, pdb->ip_hdr_len);
 	SET_LABEL(p, hdr);
 	pkeyjump = JUMP(p, keyjump, LOCAL_JUMP, ALL_TRUE, SHRD | SELF);
 	/*
@@ -712,6 +825,8 @@ static inline int cnstr_shdsc_ipsec_decap_des_aes_xcbc(uint32_t *descbuf,
 {
 	struct program prg;
 	struct program *p = &prg;
+	uint32_t ip_hdr_len = (pdb->options & PDBHDRLEN_MASK) >>
+				PDBHDRLEN_ESP_ENCAP_SHIFT;
 
 	LABEL(hdr);
 	LABEL(jump_cmd);
@@ -732,7 +847,7 @@ static inline int cnstr_shdsc_ipsec_decap_des_aes_xcbc(uint32_t *descbuf,
 
 	PROGRAM_CNTXT_INIT(p, descbuf, 0);
 	phdr = SHR_HDR(p, SHR_SERIAL, hdr, 0);
-	COPY_DATA(p, (uint8_t *)pdb, sizeof(struct ipsec_decap_pdb));
+	__rta_copy_ipsec_decap_pdb(p, pdb, cipherdata->algtype);
 	SET_LABEL(p, hdr);
 	pkeyjump = JUMP(p, keyjump, LOCAL_JUMP, ALL_TRUE, SHRD | SELF);
 	/*
@@ -754,14 +869,14 @@ static inline int cnstr_shdsc_ipsec_decap_des_aes_xcbc(uint32_t *descbuf,
 	KEY(p, KEY1, authdata->key_enc_flags, authdata->key, authdata->keylen,
 	    INLINE_KEY(authdata));
 	MATHB(p, SEQINSZ, SUB,
-	      (uint64_t)(pdb->ip_hdr_len + IPSEC_ICV_MD5_TRUNC_SIZE),
-	      MATH0, 4, IMMED2);
+	      (uint64_t)(ip_hdr_len + IPSEC_ICV_MD5_TRUNC_SIZE), MATH0, 4,
+	      IMMED2);
 	MATHB(p, MATH0, SUB, ZERO, VSEQINSZ, 4, 0);
 	ALG_OPERATION(p, OP_ALG_ALGSEL_MD5, OP_ALG_AAI_HMAC_PRECOMP,
 		      OP_ALG_AS_INITFINAL, ICV_CHECK_DISABLE, DIR_ENC);
 	ALG_OPERATION(p, OP_ALG_ALGSEL_AES, OP_ALG_AAI_XCBC_MAC,
 		      OP_ALG_AS_INITFINAL, ICV_CHECK_ENABLE, DIR_DEC);
-	SEQFIFOLOAD(p, SKIP, pdb->ip_hdr_len, 0);
+	SEQFIFOLOAD(p, SKIP, ip_hdr_len, 0);
 	SEQFIFOLOAD(p, MSG1, 0, VLF | FLUSH1);
 	SEQFIFOLOAD(p, ICV1, IPSEC_ICV_MD5_TRUNC_SIZE, FLUSH1 | LAST1);
 	/* Swap SEQOUTPTR to SEQINPTR. */
@@ -788,10 +903,10 @@ static inline int cnstr_shdsc_ipsec_decap_des_aes_xcbc(uint32_t *descbuf,
 	WORD(p, 0xA00000f3);
 	SEQINPTR(p, 0, 65535, RTO);
 	MATHB(p, MATH0, SUB, ZERO, VSEQINSZ, 4, 0);
-	MATHB(p, MATH0, ADD, pdb->ip_hdr_len, VSEQOUTSZ, 4, IMMED2);
+	MATHB(p, MATH0, ADD, ip_hdr_len, VSEQOUTSZ, 4, IMMED2);
 	move_jump = MOVE(p, DESCBUF, 0, OFIFO, 0, 8, WAITCOMP | IMMED);
 	move_jump_back = MOVE(p, OFIFO, 0, DESCBUF, 0, 8, IMMED);
-	SEQFIFOLOAD(p, SKIP, pdb->ip_hdr_len, 0);
+	SEQFIFOLOAD(p, SKIP, ip_hdr_len, 0);
 	SEQFIFOLOAD(p, MSG2, 0, VLF | LAST2);
 	SEQFIFOSTORE(p, SKIP, 0, 0, VLF);
 	SEQSTORE(p, CONTEXT2, 0, IPSEC_ICV_MD5_TRUNC_SIZE, 0);
@@ -803,8 +918,8 @@ static inline int cnstr_shdsc_ipsec_decap_des_aes_xcbc(uint32_t *descbuf,
 	     4, 0);
 	SEQINPTR(p, 0, 65535, RTO);
 	MATHB(p, MATH0, ADD,
-	      (uint64_t)(pdb->ip_hdr_len + IPSEC_ICV_MD5_TRUNC_SIZE),
-	      SEQINSZ, 4, IMMED2);
+	      (uint64_t)(ip_hdr_len + IPSEC_ICV_MD5_TRUNC_SIZE), SEQINSZ, 4,
+	      IMMED2);
 	KEY(p, KEY1, cipherdata->key_enc_flags, cipherdata->key,
 	    cipherdata->keylen, INLINE_KEY(cipherdata));
 	PROTOCOL(p, OP_TYPE_DECAP_PROTOCOL, OP_PCLID_IPSEC,
@@ -913,7 +1028,7 @@ static inline int cnstr_shdsc_ipsec_new_encap(uint32_t *descbuf, bool ps,
 		PROGRAM_SET_36BIT_ADDR(p);
 	phdr = SHR_HDR(p, SHR_SERIAL, hdr, 0);
 
-	COPY_DATA(p, (uint8_t *)pdb, sizeof(struct ipsec_encap_pdb));
+	__rta_copy_ipsec_encap_pdb(p, pdb, cipherdata->algtype);
 
 	switch (pdb->options & PDBOPTS_ESP_OIHI_MASK) {
 	case PDBOPTS_ESP_OIHI_PDB_INL:
@@ -1010,7 +1125,7 @@ static inline int cnstr_shdsc_ipsec_new_decap(uint32_t *descbuf, bool ps,
 	if (ps)
 		PROGRAM_SET_36BIT_ADDR(p);
 	phdr = SHR_HDR(p, SHR_SERIAL, hdr, 0);
-	COPY_DATA(p, (uint8_t *)pdb, sizeof(struct ipsec_decap_pdb));
+	__rta_copy_ipsec_decap_pdb(p, pdb, cipherdata->algtype);
 	SET_LABEL(p, hdr);
 	pkeyjmp = JUMP(p, keyjmp, LOCAL_JUMP, ALL_TRUE, SHRD);
 	if (authdata->keylen)

sec/rta/include/flib/compat.h
@@ -115,10 +115,23 @@ typedef unsigned char			_Bool;
 #define swab16(x) bswap_16(x)
 #define swab32(x) bswap_32(x)
 #define swab64(x) bswap_64(x)
+#if !defined(cpu_to_be32) && !defined(cpu_to_le32)
+#ifdef __BIG_ENDIAN
+#define cpu_to_be32(x)	(x)
+#define cpu_to_le32(x)	swab32(x)
+#elif defined(__LITTLE_ENDIAN)
+#define cpu_to_be32(x)	swab32(x)
+#define cpu_to_le32(x)	(x)
+#else
+#error Endianness not set in environment!
+#endif /* __BIG_ENDIAN */
+#endif /* !defined(cpu_to_be32) && !defined(cpu_to_le32) */
 #elif defined(__EWL__) && (defined(AIOP) || defined(MC))
 #define swab16(x) swap_uint16(x)
 #define swab32(x) swap_uint32(x)
 #define swab64(x) swap_uint64(x)
+#define cpu_to_be32(x)	CPU_TO_BE32(x)
+#define cpu_to_le32(x)	CPU_TO_LE32(x)
 #endif
 
 #endif /* __RTA_COMPAT_H__ */

sec/rta/include/flib/rta.h
@@ -808,8 +808,7 @@ static inline unsigned rta_get_sec_era(void)
  *
  * Return: 0 in case of success, a negative error code if it fails
  */
-#define PATCH_JUMP(program, line, new_ref) \
-	rta_patch_jmp(program, line, new_ref, false)
+#define PATCH_JUMP(program, line, new_ref) rta_patch_jmp(program, line, new_ref)
 
 /**
  * PATCH_JUMP_NON_LOCAL - Auxiliary command to resolve referential code between
@@ -826,7 +825,7 @@ static inline unsigned rta_get_sec_era(void)
  * Return: 0 in case of success, a negative error code if it fails
  */
 #define PATCH_JUMP_NON_LOCAL(src_program, line, new_ref) \
-	rta_patch_jmp(src_program, line, new_ref, true)
+	rta_patch_jmp(src_program, line, new_ref)
 
 /**
  * PATCH_MOVE - Auxiliary command to resolve self referential code
@@ -842,7 +841,7 @@ static inline unsigned rta_get_sec_era(void)
  * Return: 0 in case of success, a negative error code if it fails
  */
 #define PATCH_MOVE(program, line, new_ref) \
-	rta_patch_move(program, line, new_ref, false)
+	rta_patch_move(program, line, new_ref)
 
 /**
  * PATCH_MOVE_NON_LOCAL - Auxiliary command to resolve referential code between
@@ -860,7 +859,7 @@ static inline unsigned rta_get_sec_era(void)
  * Return: 0 in case of success, a negative error code if it fails
  */
 #define PATCH_MOVE_NON_LOCAL(src_program, line, new_ref) \
-	rta_patch_move(src_program, line, new_ref, true)
+	rta_patch_move(src_program, line, new_ref)
 
 /**
  * PATCH_LOAD - Auxiliary command to resolve self referential code
@@ -892,7 +891,7 @@ static inline unsigned rta_get_sec_era(void)
  * Return: 0 in case of success, a negative error code if it fails
  */
 #define PATCH_STORE(program, line, new_ref) \
-	rta_patch_store(program, line, new_ref, false)
+	rta_patch_store(program, line, new_ref)
 
 /**
  * PATCH_STORE_NON_LOCAL - Auxiliary command to resolve referential code between
@@ -910,7 +909,7 @@ static inline unsigned rta_get_sec_era(void)
  * Return: 0 in case of success, a negative error code if it fails
  */
 #define PATCH_STORE_NON_LOCAL(src_program, line, new_ref) \
-	rta_patch_store(src_program, line, new_ref, true)
+	rta_patch_store(src_program, line, new_ref)
 
 /**
  * PATCH_HDR - Auxiliary command to resolve self referential code
@@ -926,7 +925,7 @@ static inline unsigned rta_get_sec_era(void)
  * Return: 0 in case of success, a negative error code if it fails
  */
 #define PATCH_HDR(program, line, new_ref) \
-	rta_patch_header(program, line, new_ref, false)
+	rta_patch_header(program, line, new_ref)
 
 /**
  * PATCH_HDR_NON_LOCAL - Auxiliary command to resolve referential code between
@@ -944,7 +943,7 @@ static inline unsigned rta_get_sec_era(void)
  * Return: 0 in case of success, a negative error code if it fails
  */
 #define PATCH_HDR_NON_LOCAL(src_program, line, new_ref) \
-	rta_patch_header(src_program, line, new_ref, true)
+	rta_patch_header(src_program, line, new_ref)
 
 /**
  * PATCH_RAW - Auxiliary command to resolve self referential code
@@ -961,7 +960,7 @@ static inline unsigned rta_get_sec_era(void)
  * Return: 0 in case of success, a negative error code if it fails
  */
 #define PATCH_RAW(program, line, mask, new_val) \
-	rta_patch_raw(program, line, mask, new_val, false)
+	rta_patch_raw(program, line, mask, new_val)
 
 /**
  * PATCH_RAW_NON_LOCAL - Auxiliary command to resolve referential code between
@@ -979,6 +978,6 @@ static inline unsigned rta_get_sec_era(void)
  * Return: 0 in case of success, a negative error code if it fails
  */
 #define PATCH_RAW_NON_LOCAL(src_program, line, mask, new_val) \
-	rta_patch_raw(src_program, line, mask, new_val, true)
+	rta_patch_raw(src_program, line, mask, new_val)
 
 #endif /* __RTA_RTA_H__ */

sec/rta/include/flib/rta/sec_run_time_asm.h
@@ -383,14 +383,6 @@ static inline void rta_program_cntxt_init(struct program *program,
 	program->bswap = false;
 }
 
-static inline void __rta__desc_bswap(uint32_t *buff, unsigned buff_len)
-{
-	unsigned i;
-
-	for (i = 0; i < buff_len; i++)
-		buff[i] = swab32(buff[i]);
-}
-
 static inline int rta_program_finalize(struct program *program)
 {
 	/* Descriptor is usually not allowed to go beyond 64 words size */
@@ -404,17 +396,16 @@ static inline int rta_program_finalize(struct program *program)
 	}
 
 	/* Update descriptor length in shared and job descriptor headers */
-	if (program->shrhdr != NULL) {
-		*program->shrhdr |= program->current_pc;
-		if (program->bswap)
-			__rta__desc_bswap(program->shrhdr, program->current_pc);
-	} else if (program->jobhdr != NULL) {
-		*program->jobhdr |= program->current_pc;
-		if (program->bswap)
-			__rta__desc_bswap(program->jobhdr, program->current_pc);
-	} else {
+	if (program->shrhdr != NULL)
+		*program->shrhdr |= program->bswap ?
+					swab32(program->current_pc) :
+					program->current_pc;
+	else if (program->jobhdr != NULL)
+		*program->jobhdr |= program->bswap ?
+					swab32(program->current_pc) :
+					program->current_pc;
+	else
 		return -EINVAL;
-	}
 
 	return (int)program->current_pc;
 }
@@ -433,7 +424,20 @@ static inline unsigned rta_program_set_bswap(struct program *program)
 
 static inline void __rta_out32(struct program *program, uint32_t val)
 {
-	program->buffer[program->current_pc] = val;
+	program->buffer[program->current_pc] = program->bswap ?
+						swab32(val) : val;
+	program->current_pc++;
+}
+
+static inline void __rta_out_be32(struct program *program, uint32_t val)
+{
+	program->buffer[program->current_pc] = cpu_to_be32(val);
+	program->current_pc++;
+}
+
+static inline void __rta_out_le32(struct program *program, uint32_t val)
+{
+	program->buffer[program->current_pc] = cpu_to_le32(val);
 	program->current_pc++;
 }
 
@@ -526,10 +530,10 @@ static inline unsigned rta_set_label(struct program *program)
 }
 
 static inline int rta_patch_move(struct program *program, int line,
-				 unsigned new_ref, bool check_swap)
+				 unsigned new_ref)
 {
 	uint32_t opcode;
-	bool bswap = check_swap && program->bswap;
+	bool bswap = program->bswap;
 
 	if (line < 0)
 		return -EINVAL;
@@ -544,10 +548,10 @@ static inline int rta_patch_move(struct program *program, int line,
 }
 
 static inline int rta_patch_jmp(struct program *program, int line,
-				unsigned new_ref, bool check_swap)
+				unsigned new_ref)
 {
 	uint32_t opcode;
-	bool bswap = check_swap && program->bswap;
+	bool bswap = program->bswap;
 
 	if (line < 0)
 		return -EINVAL;
@@ -562,10 +566,10 @@ static inline int rta_patch_jmp(struct program *program, int line,
 }
 
 static inline int rta_patch_header(struct program *program, int line,
-				   unsigned new_ref, bool check_swap)
+				   unsigned new_ref)
 {
 	uint32_t opcode;
-	bool bswap = check_swap && program->bswap;
+	bool bswap = program->bswap;
 
 	if (line < 0)
 		return -EINVAL;
@@ -601,10 +605,10 @@ static inline int rta_patch_load(struct program *program, int line,
 }
 
 static inline int rta_patch_store(struct program *program, int line,
-				  unsigned new_ref, bool check_swap)
+				  unsigned new_ref)
 {
 	uint32_t opcode;
-	bool bswap = check_swap && program->bswap;
+	bool bswap = program->bswap;
 
 	if (line < 0)
 		return -EINVAL;
@@ -632,11 +636,10 @@ static inline int rta_patch_store(struct program *program, int line,
 }
 
 static inline int rta_patch_raw(struct program *program, int line,
-				unsigned mask, unsigned new_val,
-				bool check_swap)
+				unsigned mask, unsigned new_val)
 {
 	uint32_t opcode;
-	bool bswap = check_swap && program->bswap;
+	bool bswap = program->bswap;
 
 	if (line < 0)
 		return -EINVAL;

commit e4541545ee2599930cbf6ba697514c794d8c74e9

IPsec - fix compilation error
__rta_out64() takes three parameters, not two.

Change-Id: I0ec2ea86a58617a72734dc1c9f53b036b3a7b725
Fixes: 32be2d0f09f70 ("IPsec - fix PDB endianness")
Signed-off-by: Horia Geanta <horia.geanta@freescale.com>
Reviewed-on: http://git.am.freescale.net:8181/28181
Reviewed-by: Alexandru Porosanu <alexandru.porosanu@freescale.com>

sec/rta/include/flib/desc/ipsec.h
@@ -343,7 +343,7 @@ static inline unsigned __rta_copy_ipsec_encap_pdb(struct program *program,
 		rta_copy_data(program, pdb->ctr.ctr_nonce,
 			      sizeof(pdb->ctr.ctr_nonce));
 		__rta_out32(program, pdb->ctr.ctr_initial);
-		__rta_out64(program, pdb->ctr.iv);
+		__rta_out64(program, true, pdb->ctr.iv);
 		break;
 
 	case OP_PCL_IPSEC_AES_CCM8:
@@ -351,7 +351,7 @@ static inline unsigned __rta_copy_ipsec_encap_pdb(struct program *program,
 	case OP_PCL_IPSEC_AES_CCM16:
 		rta_copy_data(program, pdb->ccm.salt, sizeof(pdb->ccm.salt));
 		__rta_out32(program, pdb->ccm.ccm_opt);
-		__rta_out64(program, pdb->ccm.iv);
+		__rta_out64(program, true, pdb->ccm.iv);
 		break;
 
 	case OP_PCL_IPSEC_AES_GCM8:
@@ -360,7 +360,7 @@ static inline unsigned __rta_copy_ipsec_encap_pdb(struct program *program,
 	case OP_PCL_IPSEC_AES_NULL_WITH_GMAC:
 		rta_copy_data(program, pdb->gcm.salt, sizeof(pdb->gcm.salt));
 		__rta_out32(program, pdb->gcm.rsvd);
-		__rta_out64(program, pdb->gcm.iv);
+		__rta_out64(program, true, pdb->gcm.iv);
 		break;
 	}
 