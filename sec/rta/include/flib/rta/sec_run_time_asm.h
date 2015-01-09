nxp-qoriq/flib

commit f71cb8b74ab3536fe1bea195c608ad67856316a7

update PROGRAM_FINALIZE to return error when appropriate
PROGRAM_FINALIZE returns "unsigned int".
Tis causes an implicit cast, since most of the functions that create
descriptors have a return type of "int", but do
"return PROGRAM_FINALIZE(p);".

Update PROGRAM_FINALIZE's return type to "int".

Change-Id: I2f36a467187798bd6f36d6d79059c88b5ae68263
Signed-off-by: Horia Geanta <horia.geanta@freescale.com>
Reviewed-on: http://git.am.freescale.net:8181/18845
Reviewed-by: Alexandru Porosanu <alexandru.porosanu@freescale.com>

sec/rta/include/flib/rta/sec_run_time_asm.h
@@ -387,15 +387,17 @@ static inline void __rta__desc_bswap(uint32_t *buff, unsigned buff_len)
 		buff[i] = swab32(buff[i]);
 }
 
-static inline unsigned rta_program_finalize(struct program *program)
+static inline int rta_program_finalize(struct program *program)
 {
-	/* Descriptor is not allowed to go beyond 64 words size */
+	/* Descriptor is usually not allowed to go beyond 64 words size */
 	if (program->current_pc > MAX_CAAM_DESCSIZE)
-		pr_err("Descriptor Size exceeded max limit of 64 words\n");
+		pr_warn("Descriptor Size exceeded max limit of 64 words\n");
 
 	/* Descriptor is erroneous */
-	if (program->first_error_pc)
+	if (program->first_error_pc) {
 		pr_err("Descriptor creation error\n");
+		return -EINVAL;
+	}
 
 	/* Update descriptor length in shared and job descriptor headers */
 	if (program->shrhdr != NULL) {
@@ -406,9 +408,11 @@ static inline unsigned rta_program_finalize(struct program *program)
 		*program->jobhdr |= program->current_pc;
 		if (program->bswap)
 			__rta__desc_bswap(program->jobhdr, program->current_pc);
+	} else {
+		return -EINVAL;
 	}
 
-	return program->current_pc;
+	return (int)program->current_pc;
 }
 
 static inline unsigned rta_program_set_36bit_addr(struct program *program)

commit 5c6f9ade376e1ef99cb61af86fb4343ea825935a

fix spurious regression tests failures
Some of the regression tests (sec/rta/tests/) are currently failing:
cd sec/rta/tests && make check
[...]
Checking tests...
P4080R2_ppp_encap failed
test_operation_pkha failed
test_fifoload failed
test_nfifo failed
if_commands failed
test_store failed
test_move failed
P4080R2_ppp_decap failed
test_protocol failed
test_signature failed
test_fifostore failed
jump_commands failed
test_load failed
test_math failed
test_operation_alg failed
raid_xor_opt failed
Done!
Total  = 41
Passed = 25
Failed = 16

All failures are due to the following error in *.desc log files:
Invalid descriptor size (-22)

This happens due to PROGRAM_FINALIZE complaining for cases when
descriptors don't start with a HEADER command.
It's an unusual case *not* to start with this command. However,
(at least) for testing purposes, it has to be supported.

Fixes: f71cb8b74ab35 ("update PROGRAM_FINALIZE to return error when appropriate")
Change-Id: I74c1b2921817c57cffb0d92ec74919ee8561181c
Signed-off-by: Horia Geanta <horia.geanta@freescale.com>
Reviewed-on: http://git.am.freescale.net:8181/28168

sec/rta/include/flib/rta/sec_run_time_asm.h
@@ -404,8 +404,6 @@ static inline int rta_program_finalize(struct program *program)
 		*program->jobhdr |= program->bswap ?
 					swab32(program->current_pc) :
 					program->current_pc;
-	else
-		return -EINVAL;
 
 	return (int)program->current_pc;
 }