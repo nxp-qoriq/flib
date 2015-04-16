nxp-qoriq/flib

commit 6e7ef07c6067e22d545daf04ac3220260c378511

add Derived Key Protocol (DKP) support
DKP provides a mechanism for "deriving" the MDHA split key from
an initial ("negotiated") authentication key.
Both initial key and derived key are unencrypted.

DKP is supported in SEC Era 6+.

Change-Id: If2b11f885099e7fc8e5c1a7c9c83f877b1279492
Signed-off-by: Horia Geanta <horia.geanta@freescale.com>
Reviewed-on: http://git.am.freescale.net:8181/28928
Reviewed-by: Alexandru Porosanu <alexandru.porosanu@freescale.com>

sec/rta/include/flib/rta/protocol_cmd.h
@@ -435,6 +435,29 @@ static inline int __rta_rsa_dec_proto(uint16_t protoinfo)
 	return 0;
 }
 
+/*
+ * DKP Protocol - Restrictions on key (SRC,DST) combinations
+ * For e.g. key_in_out[0][0] = 1 means (SRC=IMM,DST=IMM) combination is allowed
+ */
+static const uint8_t key_in_out[4][4] = {1, 0, 0, 0,
+					 1, 1, 1, 1,
+					 1, 0, 1, 0,
+					 1, 0, 0, 1};
+
+static inline int __rta_dkp_proto(uint16_t protoinfo)
+{
+	int key_src = (protoinfo & OP_PCL_DKP_SRC_MASK) >> OP_PCL_DKP_SRC_SHIFT;
+	int key_dst = (protoinfo & OP_PCL_DKP_DST_MASK) >> OP_PCL_DKP_DST_SHIFT;
+
+	if (!key_in_out[key_src][key_dst]) {
+		pr_err("PROTO_DESC: Invalid DKP key (SRC,DST)\n");
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+
 static inline int __rta_3g_dcrc_proto(uint16_t protoinfo)
 {
 	if (rta_sec_era == RTA_SEC_ERA_7)
@@ -541,9 +564,15 @@ static const struct proto_map proto_table[] = {
 	{OP_TYPE_DECAP_PROTOCOL, OP_PCLID_3G_RLC_SDU,    __rta_3g_rlc_proto},
 	{OP_TYPE_DECAP_PROTOCOL, OP_PCLID_LTE_PDCP_USER, __rta_lte_pdcp_proto},
 /*29*/	{OP_TYPE_DECAP_PROTOCOL, OP_PCLID_LTE_PDCP_CTRL, __rta_lte_pdcp_proto},
+	{OP_TYPE_UNI_PROTOCOL,   OP_PCLID_DKP_MD5,       __rta_dkp_proto},
+	{OP_TYPE_UNI_PROTOCOL,   OP_PCLID_DKP_SHA1,      __rta_dkp_proto},
+	{OP_TYPE_UNI_PROTOCOL,   OP_PCLID_DKP_SHA224,    __rta_dkp_proto},
+	{OP_TYPE_UNI_PROTOCOL,   OP_PCLID_DKP_SHA256,    __rta_dkp_proto},
+	{OP_TYPE_UNI_PROTOCOL,   OP_PCLID_DKP_SHA384,    __rta_dkp_proto},
+/*35*/	{OP_TYPE_UNI_PROTOCOL,   OP_PCLID_DKP_SHA512,    __rta_dkp_proto},
 	{OP_TYPE_DECAP_PROTOCOL, OP_PCLID_PUBLICKEYPAIR, __rta_dlc_proto},
-/*31*/	{OP_TYPE_DECAP_PROTOCOL, OP_PCLID_DSASIGN,	 __rta_dlc_proto},
-/*32*/	{OP_TYPE_DECAP_PROTOCOL, OP_PCLID_LTE_PDCP_CTRL_MIXED,
+/*37*/	{OP_TYPE_DECAP_PROTOCOL, OP_PCLID_DSASIGN,	 __rta_dlc_proto},
+/*38*/	{OP_TYPE_DECAP_PROTOCOL, OP_PCLID_LTE_PDCP_CTRL_MIXED,
 	 __rta_lte_pdcp_mixed_proto},
 	{OP_TYPE_DECAP_PROTOCOL, OP_PCLID_IPSEC_NEW,     __rta_ipsec_proto},
 };
@@ -552,7 +581,7 @@ static const struct proto_map proto_table[] = {
  * Allowed OPERATION protocols for each SEC Era.
  * Values represent the number of entries from proto_table[] that are supported.
  */
-static const unsigned proto_table_sz[] = {21, 29, 29, 29, 29, 29, 31, 33};
+static const unsigned proto_table_sz[] = {21, 29, 29, 29, 29, 35, 37, 39};
 
 static inline int rta_proto_operation(struct program *program, uint32_t optype,
 				      uint32_t protid, uint16_t protoinfo)
@@ -600,6 +629,52 @@ static inline int rta_proto_operation(struct program *program, uint32_t optype,
 	program->first_error_pc = start_pc;
 	program->current_instruction++;
 	return ret;
+}
+
+static inline int rta_dkp_proto(struct program *program, uint32_t protid,
+				uint16_t key_src, uint16_t key_dst,
+				uint16_t keylen, uint64_t key,
+				enum rta_data_type key_type)
+{
+	unsigned start_pc = program->current_pc;
+	unsigned in_words = 0, out_words = 0;
+	int ret;
+
+	key_src &= OP_PCL_DKP_SRC_MASK;
+	key_dst &= OP_PCL_DKP_DST_MASK;
+	keylen &= OP_PCL_DKP_KEY_MASK;
+
+	ret = rta_proto_operation(program, OP_TYPE_UNI_PROTOCOL, protid,
+				  key_src | key_dst | keylen);
+	if (ret < 0)
+		return ret;
+
+	if ((key_src == OP_PCL_DKP_SRC_PTR) ||
+	    (key_src == OP_PCL_DKP_SRC_SGF)) {
+		__rta_out64(program, program->ps, key);
+		in_words = program->ps ? 2 : 1;
+	} else if (key_src == OP_PCL_DKP_SRC_IMM) {
+		__rta_inline_data(program, key, inline_flags(key_type), keylen);
+		in_words = (unsigned)((keylen + 3) / 4);
+	}
+
+	if ((key_dst == OP_PCL_DKP_DST_PTR) ||
+	    (key_dst == OP_PCL_DKP_DST_SGF)) {
+		out_words = in_words;
+	} else  if (key_dst == OP_PCL_DKP_DST_IMM) {
+		out_words = split_key_len(protid) / 4;
+	}
+
+	if (out_words < in_words) {
+		pr_err("PROTO_DESC: DKP doesn't currently support a smaller descriptor\n");
+		program->first_error_pc = start_pc;
+		return -EINVAL;
+	}
+
+	/* If needed, reserve space in resulting descriptor for derived key */
+	program->current_pc += (out_words - in_words);
+
+	return (int)start_pc;
 }
 
 #endif /* __RTA_PROTOCOL_CMD_H__ */

sec/rta/doc/rta-api-userspace.tmpl
@@ -221,6 +221,8 @@ full user manual separately; part of it is embedded in this SGML template.
 !Fdrivers/crypto/caam/flib/rta.h SEQOUTPTR
 !Fdrivers/crypto/caam/flib/rta.h ALG_OPERATION
 !Fdrivers/crypto/caam/flib/rta.h PROTOCOL
+!Fdrivers/crypto/caam/flib/rta/sec_run_time_asm.h rta_data_type
+!Fdrivers/crypto/caam/flib/rta.h DKP_PROTOCOL
 !Fdrivers/crypto/caam/flib/rta.h PKHA_OPERATION
 !Fdrivers/crypto/caam/flib/rta/sec_run_time_asm.h rta_jump_cond
 !Fdrivers/crypto/caam/flib/rta/sec_run_time_asm.h rta_jump_type

sec/rta/doc/rta-api.tmpl
@@ -185,6 +185,8 @@
 !Fdrivers/crypto/caam/flib/rta.h SEQOUTPTR
 !Fdrivers/crypto/caam/flib/rta.h ALG_OPERATION
 !Fdrivers/crypto/caam/flib/rta.h PROTOCOL
+!Fdrivers/crypto/caam/flib/rta/sec_run_time_asm.h rta_data_type
+!Fdrivers/crypto/caam/flib/rta.h DKP_PROTOCOL
 !Fdrivers/crypto/caam/flib/rta.h PKHA_OPERATION
 !Fdrivers/crypto/caam/flib/rta/sec_run_time_asm.h rta_jump_cond
 !Fdrivers/crypto/caam/flib/rta/sec_run_time_asm.h rta_jump_type

sec/rta/include/flib/desc.h
@@ -593,6 +593,12 @@
 #define OP_PCLID_DIFFIEHELLMAN	(0x17 << OP_PCLID_SHIFT)
 #define OP_PCLID_RSAENCRYPT	(0x18 << OP_PCLID_SHIFT)
 #define OP_PCLID_RSADECRYPT	(0x19 << OP_PCLID_SHIFT)
+#define OP_PCLID_DKP_MD5	(0x20 << OP_PCLID_SHIFT)
+#define OP_PCLID_DKP_SHA1	(0x21 << OP_PCLID_SHIFT)
+#define OP_PCLID_DKP_SHA224	(0x22 << OP_PCLID_SHIFT)
+#define OP_PCLID_DKP_SHA256	(0x23 << OP_PCLID_SHIFT)
+#define OP_PCLID_DKP_SHA384	(0x24 << OP_PCLID_SHIFT)
+#define OP_PCLID_DKP_SHA512	(0x25 << OP_PCLID_SHIFT)
 
 /* Assuming OP_TYPE = OP_TYPE_DECAP_PROTOCOL/ENCAP_PROTOCOL */
 #define OP_PCLID_IPSEC		(0x01 << OP_PCLID_SHIFT)
@@ -1422,6 +1428,22 @@
 #define OP_PCL_RSAPROT_PPP_EKT		(3 << OP_PCL_RSAPROT_PPP_SHIFT)
 #define OP_PCL_RSAPROT_PPP_TK_EKT	(7 << OP_PCL_RSAPROT_PPP_SHIFT)
 #define OP_PCL_RSAPROT_FMT_PKCSV15	BIT(12)
+
+/* Derived Key Protocol (DKP) Protinfo */
+#define OP_PCL_DKP_SRC_SHIFT	14
+#define OP_PCL_DKP_SRC_MASK	(3 << OP_PCL_DKP_SRC_SHIFT)
+#define OP_PCL_DKP_SRC_IMM	(0 << OP_PCL_DKP_SRC_SHIFT)
+#define OP_PCL_DKP_SRC_SEQ	(1 << OP_PCL_DKP_SRC_SHIFT)
+#define OP_PCL_DKP_SRC_PTR	(2 << OP_PCL_DKP_SRC_SHIFT)
+#define OP_PCL_DKP_SRC_SGF	(3 << OP_PCL_DKP_SRC_SHIFT)
+#define OP_PCL_DKP_DST_SHIFT	12
+#define OP_PCL_DKP_DST_MASK	(3 << OP_PCL_DKP_DST_SHIFT)
+#define OP_PCL_DKP_DST_IMM	(0 << OP_PCL_DKP_DST_SHIFT)
+#define OP_PCL_DKP_DST_SEQ	(1 << OP_PCL_DKP_DST_SHIFT)
+#define OP_PCL_DKP_DST_PTR	(2 << OP_PCL_DKP_DST_SHIFT)
+#define OP_PCL_DKP_DST_SGF	(3 << OP_PCL_DKP_DST_SHIFT)
+#define OP_PCL_DKP_KEY_SHIFT	0
+#define OP_PCL_DKP_KEY_MASK	(0xfff << OP_PCL_DKP_KEY_SHIFT)
 
 /* For non-protocol/alg-only op commands */
 #define OP_ALG_TYPE_SHIFT	24

sec/rta/include/flib/desc/common.h
@@ -16,24 +16,6 @@
  */
 
 /**
- * enum rta_data_type - Indicates how is the data provided and how to include it
- *                      in the descriptor.
- * @RTA_DATA_PTR: Data is in memory and accessed by reference; data address is a
- *               physical (bus) address.
- * @RTA_DATA_IMM: Data is inlined in descriptor and accessed as immediate data;
- *               data address is a virtual address.
- * @RTA_DATA_IMM_DMA: (AIOP only) Data is inlined in descriptor and accessed as
- *                   immediate data; data address is a physical (bus) address
- *                   in external memory and CDMA is programmed to transfer the
- *                   data into descriptor buffer being built in Workspace Area.
- */
-enum rta_data_type {
-	RTA_DATA_PTR = 1,
-	RTA_DATA_IMM,
-	RTA_DATA_IMM_DMA
-};
-
-/**
  * struct alginfo - Container for algorithm details
  * @algtype: algorithm selector; for valid values, see documentation of the
  *           functions where it is used.
@@ -52,22 +34,6 @@ struct alginfo {
 	uint32_t key_enc_flags;
 	enum rta_data_type key_type;
 };
-
-static inline uint32_t inline_flags(enum rta_data_type data_type)
-{
-	switch (data_type) {
-	case RTA_DATA_PTR:
-		return 0;
-	case RTA_DATA_IMM:
-		return IMMED | COPY;
-	case RTA_DATA_IMM_DMA:
-		return IMMED | DCOPY;
-	default:
-		/* warn and default to RTA_DATA_PTR */
-		pr_warn("RTA: defaulting to RTA_DATA_PTR parameter type\n");
-		return 0;
-	}
-}
 
 #define INLINE_KEY(alginfo)	inline_flags(alginfo->key_type)
 
@@ -121,35 +87,5 @@ struct protcmd {
 	uint32_t protid;
 	uint16_t protinfo;
 };
-
-/**
- * split_key_len - Compute MDHA split key length for a given algorithm
- * @hash: Hashing algorithm selection, one of OP_ALG_ALGSEL_* - MD5, SHA1,
- *        SHA224, SHA384, SHA512.
- *
- * Return: MDHA split key length
- */
-static inline uint32_t split_key_len(uint32_t hash)
-{
-	/* Sizes for MDHA pads (*not* keys): MD5, SHA1, 224, 256, 384, 512 */
-	static const uint8_t mdpadlen[] = { 16, 20, 32, 32, 64, 64 };
-	uint32_t idx;
-
-	idx = (hash & OP_ALG_ALGSEL_SUBMASK) >> OP_ALG_ALGSEL_SHIFT;
-
-	return (uint32_t)(mdpadlen[idx] * 2);
-}
-
-/**
- * split_key_pad_len - Compute MDHA split key pad length for a given algorithm
- * @hash: Hashing algorithm selection, one of OP_ALG_ALGSEL_* - MD5, SHA1,
- *        SHA224, SHA384, SHA512.
- *
- * Return: MDHA split key pad length
- */
-static inline uint32_t split_key_pad_len(uint32_t hash)
-{
-	return ALIGN(split_key_len(hash), 16);
-}
 
 #endif /* __DESC_COMMON_H__ */

sec/rta/include/flib/rta.h
@@ -499,6 +499,30 @@ static inline unsigned rta_get_sec_era(void)
 	rta_proto_operation(program, optype, protid, protoinfo)
 
 /**
+ * DKP_PROTOCOL - Configures DKP (Derived Key Protocol) PROTOCOL command
+ * @program: pointer to struct program
+ * @protid: protocol identifier value - one of the following:
+ *          OP_PCLID_DKP_{MD5 | SHA1 | SHA224 | SHA256 | SHA384 | SHA512}
+ * @key_src: How the initial ("negotiated") key is provided to the DKP protocol.
+ *           Valid values - one of OP_PCL_DKP_SRC_{IMM, SEQ, PTR, SGF}. Not all
+ *           (key_src,key_dst) combinations are allowed.
+ * @key_dst: How the derived ("split") key is returned by the DKP protocol.
+ *           Valid values - one of OP_PCL_DKP_DST_{IMM, SEQ, PTR, SGF}. Not all
+ *           (key_src,key_dst) combinations are allowed.
+ * @keylen: length of the initial key, in bytes (uint16_t)
+ * @key: address where algorithm key resides; virtual address if key_type is
+ *       RTA_DATA_IMM, physical (bus) address if key_type is RTA_DATA_PTR or
+ *       RTA_DATA_IMM_DMA.
+ * @key_type: enum rta_data_type
+ * Return: On success, descriptor buffer offset where this command is inserted.
+ *         On error, a negative error code; first error program counter will
+ *         point to offset in descriptor buffer where the instruction should
+ *         have been written.
+ */
+#define DKP_PROTOCOL(program, protid, key_src, key_dst, keylen, key, key_type) \
+	rta_dkp_proto(program, protid, key_src, key_dst, keylen, key, key_type)
+
+/**
  * PKHA_OPERATION - Configures PKHA OPERATION command
  * @program: pointer to struct program
  * @op_pkha: PKHA operation; indicates the modular arithmetic function to

sec/rta/include/flib/rta/sec_run_time_asm.h
@@ -124,6 +124,24 @@ enum rta_share_type {
 	SHR_DEFER
 };
 
+/**
+ * enum rta_data_type - Indicates how is the data provided and how to include it
+ *                      in the descriptor.
+ * @RTA_DATA_PTR: Data is in memory and accessed by reference; data address is a
+ *               physical (bus) address.
+ * @RTA_DATA_IMM: Data is inlined in descriptor and accessed as immediate data;
+ *               data address is a virtual address.
+ * @RTA_DATA_IMM_DMA: (AIOP only) Data is inlined in descriptor and accessed as
+ *                   immediate data; data address is a physical (bus) address
+ *                   in external memory and CDMA is programmed to transfer the
+ *                   data into descriptor buffer being built in Workspace Area.
+ */
+enum rta_data_type {
+	RTA_DATA_PTR = 1,
+	RTA_DATA_IMM,
+	RTA_DATA_IMM_DMA
+};
+
 /* Registers definitions */
 enum rta_regs {
 	/* CCB Registers */
@@ -467,6 +485,22 @@ static inline unsigned rta_dword(struct program *program, uint64_t val)
 	return start_pc;
 }
 
+static inline uint32_t inline_flags(enum rta_data_type data_type)
+{
+	switch (data_type) {
+	case RTA_DATA_PTR:
+		return 0;
+	case RTA_DATA_IMM:
+		return IMMED | COPY;
+	case RTA_DATA_IMM_DMA:
+		return IMMED | DCOPY;
+	default:
+		/* warn and default to RTA_DATA_PTR */
+		pr_warn("RTA: defaulting to RTA_DATA_PTR parameter type\n");
+		return 0;
+	}
+}
+
 static inline unsigned rta_copy_data(struct program *program, uint8_t *data,
 				     unsigned length)
 {
@@ -521,6 +555,36 @@ static inline unsigned rta_desc_len(uint32_t *buffer)
 static inline unsigned rta_desc_bytes(uint32_t *buffer)
 {
 	return (unsigned)(rta_desc_len(buffer) * CAAM_CMD_SZ);
+}
+
+/**
+ * split_key_len - Compute MDHA split key length for a given algorithm
+ * @hash: Hashing algorithm selection, one of OP_ALG_ALGSEL_* or
+ *        OP_PCLID_DKP_* - MD5, SHA1, SHA224, SHA256, SHA384, SHA512.
+ *
+ * Return: MDHA split key length
+ */
+static inline uint32_t split_key_len(uint32_t hash)
+{
+	/* Sizes for MDHA pads (*not* keys): MD5, SHA1, 224, 256, 384, 512 */
+	static const uint8_t mdpadlen[] = { 16, 20, 32, 32, 64, 64 };
+	uint32_t idx;
+
+	idx = (hash & OP_ALG_ALGSEL_SUBMASK) >> OP_ALG_ALGSEL_SHIFT;
+
+	return (uint32_t)(mdpadlen[idx] * 2);
+}
+
+/**
+ * split_key_pad_len - Compute MDHA split key pad length for a given algorithm
+ * @hash: Hashing algorithm selection, one of OP_ALG_ALGSEL_* - MD5, SHA1,
+ *        SHA224, SHA384, SHA512.
+ *
+ * Return: MDHA split key pad length
+ */
+static inline uint32_t split_key_pad_len(uint32_t hash)
+{
+	return ALIGN(split_key_len(hash), 16);
 }
 
 static inline unsigned rta_set_label(struct program *program)

commit 8595be78c27122c4a3ee80940573b1f8a3d3e7e2

PROTOCOL - fix missing braces compiler warning
gcc -Wall -Wshadow -g -O0 -I../include -DSUPPRESS_PRINTS -c -o pointer_size.o pointer_size.c
In file included from ../include/flib/rta.h:20:0,
                 from pointer_size.c:8:
../include/flib/rta/protocol_cmd.h:442:1: warning: missing braces around initializer [-Wmissing-braces]
 static const uint8_t key_in_out[4][4] = {1, 0, 0, 0,
 ^
../include/flib/rta/protocol_cmd.h:442:1: warning: (near initialization for 'key_in_out[0]') [-Wmissing-braces]

Change-Id: Ib2ea28bf54e5e590afc9096a02c471abd5820ca6
Fixes: 6e7ef07c6067e ("add Derived Key Protocol (DKP) support")
Signed-off-by: Horia Geanta <horia.geanta@freescale.com>
Reviewed-on: http://git.am.freescale.net:8181/35180

sec/rta/include/flib/rta/protocol_cmd.h
@@ -439,10 +439,10 @@ static inline int __rta_rsa_dec_proto(uint16_t protoinfo)
  * DKP Protocol - Restrictions on key (SRC,DST) combinations
  * For e.g. key_in_out[0][0] = 1 means (SRC=IMM,DST=IMM) combination is allowed
  */
-static const uint8_t key_in_out[4][4] = {1, 0, 0, 0,
-					 1, 1, 1, 1,
-					 1, 0, 1, 0,
-					 1, 0, 0, 1};
+static const uint8_t key_in_out[4][4] = { {1, 0, 0, 0},
+					  {1, 1, 1, 1},
+					  {1, 0, 1, 0},
+					  {1, 0, 0, 1} };
 
 static inline int __rta_dkp_proto(uint16_t protoinfo)
 {