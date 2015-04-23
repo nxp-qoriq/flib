nxp-qoriq/flib

commit 89835611c90ce83d216fed546ad4624dac600aba

add algorithm mode support
Change-Id: I39edadaeb3b00c6aa0ff0e1f7f448d8e492095f0
Signed-off-by: Alex Porosanu <alexandru.porosanu@freescale.com>
Reviewed-on: http://git.am.freescale.net:8181/28644
Reviewed-by: Horia Ioan Geanta Neag <horia.geanta@freescale.com>
Tested-by: Horia Ioan Geanta Neag <horia.geanta@freescale.com>
(cherry picked from commit 11688af70e327b652b0f79b9cb427be17ffd7d56)
Reviewed-on: http://git.am.freescale.net:8181/31648

sec/rta/include/flib/desc/common.h
@@ -19,6 +19,8 @@
  * struct alginfo - Container for algorithm details
  * @algtype: algorithm selector; for valid values, see documentation of the
  *           functions where it is used.
+ * @algmode: algorithm mode selector; for valid values, see documentation of the
+ *           functions where it is used.
  * @keylen: length of the provided algorithm key, in bytes
  * @key: address where algorithm key resides; virtual address if key_type is
  *       RTA_DATA_IMM, physical (bus) address if key_type is RTA_DATA_PTR or
@@ -29,6 +31,7 @@
  */
 struct alginfo {
 	uint32_t algtype;
+	uint32_t algmode;
 	uint32_t keylen;
 	uint64_t key;
 	uint32_t key_enc_flags;

sec/rta/doc/rta-api-userspace.tmpl
@@ -272,7 +272,7 @@ full user manual separately; part of it is embedded in this SGML template.
 !Pdrivers/crypto/caam/flib/desc/algo.h Algorithms - Shared Descriptor Constructors
 !Fdrivers/crypto/caam/flib/desc/algo.h cnstr_shdsc_snow_f8
 !Fdrivers/crypto/caam/flib/desc/algo.h cnstr_shdsc_snow_f9
-!Fdrivers/crypto/caam/flib/desc/algo.h cnstr_shdsc_cbc_blkcipher
+!Fdrivers/crypto/caam/flib/desc/algo.h cnstr_shdsc_blkcipher
 !Fdrivers/crypto/caam/flib/desc/algo.h cnstr_shdsc_hmac
 !Fdrivers/crypto/caam/flib/desc/algo.h cnstr_shdsc_kasumi_f8
 !Fdrivers/crypto/caam/flib/desc/algo.h cnstr_shdsc_kasumi_f9

sec/rta/include/flib/desc/algo.h
@@ -103,7 +103,7 @@ static inline int cnstr_shdsc_snow_f9(uint32_t *descbuf, bool ps,
 }
 
 /**
- * cnstr_shdsc_cbc_blkcipher - CBC block cipher
+ * cnstr_shdsc_blkcipher - block cipher transformation
  * @descbuf: pointer to descriptor-under-construction buffer
  * @ps: if 36/40bit addressing is desired, this parameter must be true
  * @cipherdata: pointer to block cipher transform definitions
@@ -113,7 +113,7 @@ static inline int cnstr_shdsc_snow_f9(uint32_t *descbuf, bool ps,
  *
  * Return: size of descriptor written in words or negative number on error
  */
-static inline int cnstr_shdsc_cbc_blkcipher(uint32_t *descbuf, bool ps,
+static inline int cnstr_shdsc_blkcipher(uint32_t *descbuf, bool ps,
 			       struct alginfo *cipherdata, uint8_t *iv,
 			       uint32_t ivlen, uint8_t dir)
 {
@@ -137,7 +137,7 @@ static inline int cnstr_shdsc_cbc_blkcipher(uint32_t *descbuf, bool ps,
 	    cipherdata->keylen, INLINE_KEY(cipherdata));
 
 	if (is_aes_dec) {
-		ALG_OPERATION(p, cipherdata->algtype, OP_ALG_AAI_CBC,
+		ALG_OPERATION(p, cipherdata->algtype, cipherdata->algmode,
 			      OP_ALG_AS_INITFINAL, ICV_CHECK_DISABLE, dir);
 
 		pskipdk = JUMP(p, skipdk, LOCAL_JUMP, ALL_TRUE, 0);
@@ -145,12 +145,12 @@ static inline int cnstr_shdsc_cbc_blkcipher(uint32_t *descbuf, bool ps,
 	SET_LABEL(p, keyjmp);
 
 	if (is_aes_dec) {
-		ALG_OPERATION(p, OP_ALG_ALGSEL_AES, OP_ALG_AAI_CBC |
+		ALG_OPERATION(p, OP_ALG_ALGSEL_AES, cipherdata->algmode |
 			      OP_ALG_AAI_DK, OP_ALG_AS_INITFINAL,
 			      ICV_CHECK_DISABLE, dir);
 		SET_LABEL(p, skipdk);
 	} else {
-		ALG_OPERATION(p, cipherdata->algtype, OP_ALG_AAI_CBC,
+		ALG_OPERATION(p, cipherdata->algtype, cipherdata->algmode,
 			      OP_ALG_AS_INITFINAL, ICV_CHECK_DISABLE, dir);
 	}
 

commit 9f186491ce763446ac52b9775b6ac4d4db7403b3

convert alginfo->algmode from uint32_t to uint16_t
mwcceppc compiler complains about algmode's data type in all places
where it's used:
implicit arithmetic conversion from 'unsigned long' to 'unsigned short'

Convert it to uint16_t.
Move it inside the alginfo structure so that members align naturally.

Change-Id: Id7af36dc602db2d6976f99a03de87ae30916cbb8
Fixes: 89835611c90c ("add algorithm mode support")
Signed-off-by: Horia Geanta <horia.geanta@freescale.com>
Reviewed-on: http://git.am.freescale.net:8181/35217

sec/rta/include/flib/desc/common.h
@@ -19,8 +19,6 @@
  * struct alginfo - Container for algorithm details
  * @algtype: algorithm selector; for valid values, see documentation of the
  *           functions where it is used.
- * @algmode: algorithm mode selector; for valid values, see documentation of the
- *           functions where it is used.
  * @keylen: length of the provided algorithm key, in bytes
  * @key: address where algorithm key resides; virtual address if key_type is
  *       RTA_DATA_IMM, physical (bus) address if key_type is RTA_DATA_PTR or
@@ -28,14 +26,16 @@
  * @key_enc_flags: key encryption flags; see encrypt_flags parameter of KEY
  *                 command for valid values.
  * @key_type: enum rta_data_type
+ * @algmode: algorithm mode selector; for valid values, see documentation of the
+ *           functions where it is used.
  */
 struct alginfo {
 	uint32_t algtype;
-	uint32_t algmode;
 	uint32_t keylen;
 	uint64_t key;
 	uint32_t key_enc_flags;
 	enum rta_data_type key_type;
+	uint16_t algmode;
 };
 
 #define INLINE_KEY(alginfo)	inline_flags(alginfo->key_type)